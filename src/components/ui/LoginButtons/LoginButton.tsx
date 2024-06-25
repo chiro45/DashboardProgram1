e2236b61b0f757b1e3cd005c4efea2dcde074ab1
chiro45 <luciano.chiroli45@gmail.com>
2024-06-25 16:46:24 -0300

    prueba

--- src/components/ui/LoginButtons/LoginButton.tsx
+++ src/components/ui/LoginButtons/LoginButton.tsx
@@ -1,17 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
-    return (
-        <button
-            onClick={() => loginWithRedirect({
-                appState: { returnTo: window.location.origin },
-            })}
-            className="btn btn-primary"
-        >
-            INGRESO
-        </button>
-    );
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <button
+      onClick={() =>
+        loginWithRedirect({
+          authorizationParams: {
+            screen_hint: "login",
+          },
+        })
+      }
+      className="btn btn-primary"
+    >
+      INGRESO
+    </button>
+  );
 };
-export default LoginButton
+export default LoginButton;
